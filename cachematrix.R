a9604580b4e3961966b447b930fa17e009eb2d3c
exarc <chad.dufaud@gmail.com>
2014-06-25 00:45:16 -0400

    Completed cacheMatrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions, makeCacheMatrix and cacheSolve, that invert a square matrix
+## and cache the result for future retrieval.
 
-## Write a short comment describing this function
+## makeCacheMatrix prepares matrix for inversion by cacheSolve
+## $get returns 'x'
+## $setinvert sets cached inverted matrix value 'm' to input 'solve'
+## $getinvert returns cached inverted matrix 'm'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        get <- function() x
+        setinvert <- function(solve) m <<- solve
+        getinvert <- function() m
+        list(get = get, setinvert = setinvert, getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Given a matrix 'x', cacheSolve first checks if the inversion of 'x' has
+## already been cached and calculated, and if so, returns the cached value.
+## Otherwise, it will calculate and return the inversion of 'x'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinvert()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinvert(m)
+        m
 }
